4486512d956dc60f72776be88992debe31439cb4
Taha Kuzudişli <99493820+TahaKuzudisli@users.noreply.github.com>
2024-05-10 22:45:47 +0300

    Update slider.js

--- Activity-10/Activity10_Slider/slider.js
+++ Activity-10/Activity10_Slider/slider.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
     var slider = $("#slider").bxSlider({
         auto: true,
-        minSlides: 1,  
-        maxSlides: 1,  
+        minSlides: 1,
+        maxSlides: 1,
         slideWidth: 250,
         slideMargin: 10,
         randomStart: true,
         moveSlides: 1,
         pause: 3000,
-        pager: true,
-        pagerType: 'full',
-        pagerSelector: '#pager'
+        pager: false,  // Varsayılan pager'ı devre dışı bırak
+        onSliderLoad: function(currentIndex) {
+            updatePagerText(currentIndex + 1);  // Slider yüklendiğinde pager metnini güncelle
+        },
+        onSlideAfter: function($slideElement, oldIndex, newIndex) {
+            updatePagerText(newIndex + 1);  // Her slayt geçişinden sonra pager metnini güncelle
+        }
     });
+
+    function updatePagerText(currentIndex) {
+        var totalSlides = slider.getSlideCount();
+        $('#pager').text(currentIndex + ' / ' + totalSlides);  // Özel formatınız
+    }
 });
